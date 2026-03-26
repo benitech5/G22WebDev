f01f2b19e787a6a47bf0bc8af05f7979afefd973
Benjamin Twum <starblade1111@gmail.com>
2026-03-26 12:42:56 +0000

    Maintaining index.html

---
+++ Frontend/index.js
@@ -0,0 +1,11 @@
+function toggleLogin() {
+
+    const dropdown = document.getElementById("loginDropdown");
+
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+
+}
